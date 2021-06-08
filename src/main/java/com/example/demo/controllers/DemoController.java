e8025b0164f5b6855e600c0941b17c34e43458e9
veliValentine <nikkevanukas1995@gmail.com>
2021-06-08 17:28:32 +0300

    feat: POST /transform

--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -1,7 +1,14 @@
 package com.example.demo.controllers;
 
+import com.example.demo.service.DemoTransformer;
+import com.example.demo.service.ErrorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -10,9 +17,26 @@ class DemoController {
 
     private static final Logger log = LoggerFactory.getLogger(DemoController.class);
 
-    public DemoController() {
-    }
+    @Autowired
+    private DemoTransformer demoTransformer;
 
-    //TODO: REST API
+    @Autowired
+    private ErrorService errorService;
+
+    @PostMapping(
+            value = "/transform",
+            headers = "content-type=application/xml",
+            produces="application/xml"
+    )
+    public ResponseEntity<String> transform(@RequestBody String content) {
+        if (content.isBlank()){
+            return errorService.http400("Request body is blank");
+        }
+        String result = demoTransformer.transform(content);
+        if (result.isBlank()){
+            return errorService.http500("Could not parse given XML file");
+        }
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
 
 }
