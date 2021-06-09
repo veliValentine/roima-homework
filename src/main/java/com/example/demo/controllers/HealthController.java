60e4b68a9949d279a4783ba7d8005c7e5bc5753d
veliValentine <nikkevanukas1995@gmail.com>
2021-06-09 15:53:14 +0300

    feat: healthCheck

---
+++ src/main/java/com/example/demo/controllers/HealthController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck(){
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+}
