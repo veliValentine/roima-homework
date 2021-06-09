c97c70204abb7137b70fae8a251242dd65dfa9ce
veliValentine <nikkevanukas1995@gmail.com>
2021-06-09 09:38:39 +0300

    feat: timestamp

---
+++ src/main/java/com/example/demo/models/TimeStamp.java
@@ -0,0 +1,24 @@
+package com.example.demo.models;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+public class TimeStamp {
+
+    private static String TIME_STAMP_PATTERN = "yyyy-MM-dd hh:mm:ss:SSS";
+
+    public static String currentUTCTime(){
+        ZoneId UTC_TIME_ZONE =ZoneId.from(ZoneOffset.UTC);
+        DateTimeFormatter formatter = DateTimeFormatter
+                .ofPattern(TIME_STAMP_PATTERN)
+                .withZone(UTC_TIME_ZONE);
+        String date = formatter.format(Instant.now());
+        return convertToISO(date);
+    }
+
+    private static String convertToISO(String date) {
+        return date.replace(" ", "T") + "Z";
+    }
+}
